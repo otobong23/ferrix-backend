17efe3eb8efd0667f7d84d936a17833e6ef1690a
miracle boniface <udohmiracle23@gmail.com>
2026-02-09 21:35:24 +0100

    repo updated: removing all the controllers

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { config } from 'dotenv';
 import * as bodyParser from 'body-parser';
 config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
