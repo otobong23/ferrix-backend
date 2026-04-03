f80bf9f8ce7ef6e4a0b8f5d5c1816f5d8aed1e6e
miracle boniface <udohmiracle23@gmail.com>
2026-04-03 19:12:22 +0100

    payment system updated

--- src/common/helpers/verifiyPaymentSignature.ts
+++ src/common/helpers/verifiyPaymentSignature.ts
@@ -5,11 +5,10 @@ config()
 export function verifyPaymentsSignature(rawBody: string, signature: string) {
 
    const secret = process.env.USDT_PAYMENT_GATEWAY_SECRET!;
-   const payload = JSON.parse(rawBody);
 
    const hmac = crypto
       .createHmac("sha256", secret)
-      .update(payload.data.txHash)
+      .update(rawBody)
       .digest("hex");
 
    return hmac === signature;
